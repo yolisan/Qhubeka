18770ecf240d2113e480171d2beda37e29ba93fb
kajaMulumba <mulumbak93@gmail.com>
2020-09-14 21:14:06 +0200

    kaja commit splash

--- app/src/main/java/com/fishfingers/qhubeka/MainActivity.java
+++ app/src/main/java/com/fishfingers/qhubeka/MainActivity.java
@@ -1,8 +1,12 @@
 package com.fishfingers.qhubeka;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.fishfingers.qhubeka.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        new Intent(this, LoginActivity.class);
+
+        //close splash activity
+        finish();
+
+
     }
+
 }
